1bf985d6cadcbe2cfffcfb22aa0bc63650ea5d13
veroa8 <veronica.tangarife1@udea.edu.co>
2023-11-06 03:00:53 -0500

    Primera parte

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
 
-const Feed = () => {
+// const Feed = () => {
+//   return (
+//     <div>Feed</div>
+   
+//   )
+// }
+
+// export default Feed
+
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from './Login';
+import Card from '../components/card/card';
+import './Feed';
+
+
+const Home = () => {
+  const { accommodations } = useContext(AppContext);
+  const { accommodationsDispatch } = accommodations;
+  const { accommodationList, filteredAccommodation, activeFilter } = accommodations.accommodations;
+
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    getAccommodationsWihtCategories().then((response) => {
+      accommodationsDispatch({
+        type: 'FILL',
+        payload: response
+      })
+    })
+  }, [])
+
+
+
   return (
-    <div>Feed</div>
+    <main className='home'>
+      <button onClick={() => setShowForm(!showForm)}>Comentarios</button>
+      {
+        showForm?<div>Este es un formulario</div>:<></>
+      }
+      <Filterbutton />
+      {
+        activeFilter ? (
+          <section className='home__cards'>
+            {
+              filteredAccommodation.length ? filteredAccommodation.map(item => <Card key={item.id} accommodation={item} />) : <div>No hay resultado para esta búsqueda</div>
+            }
+          </section>
+        ) : (
+          <section className='home__cards'>
+            {
+              accommodationList.length ? (
+                accommodationList.map(item => <Card key={item.id} accommodation={item} />)
+              ) : <div>...Cargando</div>
+            }
+          </section>
+        )
+      }
+
+    </main>
   )
 }
 
-export default Feed
+export default Home
--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -4,6 +4,8 @@ const endpoints = {
     userByEmailAndPassword: (email, password) => `${URL_BASE}users?correo=${email}&&contrasena=${password}`,
     users: `${URL_BASE}users`,
     findUserByEmail: (email) => `${URL_BASE}users?correo=${email}`,
+    posts: `${URL_BASE}post`,
+    
 };
 
 export default endpoints;
