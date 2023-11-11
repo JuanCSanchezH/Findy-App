0ba12f1629a2e6f4cd0d48e2ed1c7f70218de258
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-11 18:04:01 -0500

    Update dependencies and remove unnecessary imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './routes/App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import Profile from './pages/Profile/Profile.jsx'
-import Gallery from './components/Gallery/index.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './routes/App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>,
-)
+  </React.StrictMode>,
+);
