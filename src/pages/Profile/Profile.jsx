7579197acc9b09ea428829140270013accf86695
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-11 10:20:19 -0500

    Install dependencies

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,7 +2,7 @@
 
 import { Link, useParams } from "react-router-dom";
 import Gallery from "../../components/Gallery";
-import Card from "../../components/Card";
+import Card from "../../components/Banner";
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
