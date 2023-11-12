6b67f7c1c7c4ed84a7f8ecac391a35540a95ca34
EstebanM12 <desteban.maya@udea.edu.co>
2023-11-12 16:37:54 -0500

    Update code to use functional components and add proptypes

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from "react";
-import { FaSignal } from "react-icons/fa6";
-import { MdWifi } from "react-icons/md";
-import { CiBatteryFull } from "react-icons/ci";
-import { AiOutlineLeft } from "react-icons/ai";
-import { BsThreeDots } from "react-icons/bs";
-import { getPost } from "../../services/postsServices";
+import { useState, useEffect } from 'react';
+import { FaSignal } from 'react-icons/fa6';
+import { MdWifi } from 'react-icons/md';
+import { CiBatteryFull } from 'react-icons/ci';
+import { AiOutlineLeft } from 'react-icons/ai';
+import { BsThreeDots } from 'react-icons/bs';
+import { getPost } from '../../services/postsServices';
 
-import "./main.scss";
+
+import './main.scss';
+
+import PropTypes from 'prop-types';
 
 function getCurrentTime() {
   const now = new Date();
-  const hours = now.getHours().toString().padStart(2, "0");
-  const minutes = now.getMinutes().toString().padStart(2, "0");
-  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
 
   return `${hours}:${minutes}:${seconds}`;
 }
 
-const Banner = () => {
+const Banner = ({ userId }) => {
   const [currentTime, setCurrentTime] = useState(getCurrentTime());
-  const [backgroundImage, setBackgroundImage] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState('');
   // Update the current time every second
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(getCurrentTime());
     }, 1000);
-    const userId = 1;
     getPost(userId)
       .then((response) => {
         console.log(response.posts[2].archivo);
@@ -36,41 +38,46 @@ const Banner = () => {
       });
 
     return () => clearInterval(interval);
-  }, []);
+  }, [userId]);
+
   return (
     <div
       className="banner__container"
       style={{
         backgroundImage: `url(${backgroundImage})`,
-        objectFit: "cover",
-        objectPosition: "top",
-        backgroundSize: "cover",
+        objectFit: 'cover',
+        objectPosition: 'top',
+        backgroundSize: 'cover',
       }}
     >
       <nav className="banner__nav">
         <span>{currentTime}</span>
         <div className="banner__nav-status">
           <div className="banner__nav-status-icon">
-            <FaSignal />
+            <FaSignal color="black" />
           </div>
           <div className="banner__nav-status-icon">
-            <MdWifi />
+            <MdWifi color="black" />
           </div>
           <div className="banner__nav-status-icon">
-            <CiBatteryFull />
+            <CiBatteryFull color="black" />
           </div>
         </div>
       </nav>
       <div className="banner__actions">
         <button className="banner__actions--button">
-          <AiOutlineLeft />
+          <AiOutlineLeft size={20} />
         </button>
         <button className="banner__actions--button">
-          <BsThreeDots />
+          <BsThreeDots size={20} />
         </button>
       </div>
     </div>
   );
+};
+
+Banner.propTypes = {
+  userId: PropTypes.number.isRequired,
 };
 
 export default Banner;
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,40 +2,40 @@ import "./main.scss";
 import PropTypes from 'prop-types';
 import { useState,useEffect } from "react";
 import { getPost } from "../../services/postsServices";
-const Card = ({userId}) => {
-  const [userData,setUserData] = useState([]);
-  useEffect(()=>{
-    getPost(userId)
-      .then((response) => {
-        console.log(response);
-        setUserData(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
 
-  })
-  function getNumberLikes (userData){
-    let likes = 0;
-    userData.posts.forEach(post => {
-      likes += post.likes;
+const Card = ({userId}) => {
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    getPost(userId).then((response) => {
+      console.log(response);
+      setUserData(response);
+    }).catch((error) => {
+      console.error(error);
     });
+  }, [userId]);
+
+  function getNumberLikes(userData) {
+    let likes = 0;
+    if (userData.posts) {
+      userData.posts.forEach((post) => {
+        likes += post.likes;
+      });
+    }
     return likes;
   }
 
-  const likes = getNumberLikes(userData);
   return (
     <div className="card__container">
       <nav className="card__nav">
         <div className="card__nav__followers">
-          <span>{userData.seguidores.length}M</span>
+          <span>{userData.seguidores ? userData.seguidores.length : 0}M</span>
           <span>Followers</span>
         </div>
         <div className="card__nav__profile">
           <img src={userData.avatar} alt="profile" />
         </div>
         <div className="card__nav__likes">
-          <span>{likes}M</span>
+          <span>{getNumberLikes(userData)}M</span>
           <span>Likes</span>
         </div>
       </nav>
