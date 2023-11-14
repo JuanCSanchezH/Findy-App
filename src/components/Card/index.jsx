e864633cdb59b9919968835d0aeae50510f7981d
EstebanM12 <desteban.maya@udea.edu.co>
2023-11-13 21:04:43 -0500

    Added Google Font Poppins, updated import statements, added modal, updated styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,25 @@
 import "./main.scss";
-import PropTypes from 'prop-types';
-import { useState,useEffect } from "react";
-import { getPost } from '../../services/postServices.js';
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { getPost } from "../../services/postServices.js";
 
-const Card = ({userId}) => {
+const Card = ({ userId }) => {
   const [userData, setUserData] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleProfileClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   useEffect(() => {
-    getPost(userId).then((response) => {
-      console.log(response);
-      setUserData(response);
-    }).catch((error) => {
-      console.error(error);
-    });
+    getPost(userId)
+      .then((response) => {
+        console.log(response);
+        setUserData(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, [userId]);
 
   function getNumberLikes(userData) {
@@ -32,7 +40,11 @@ const Card = ({userId}) => {
           <span>Followers</span>
         </div>
         <div className="card__nav__profile">
-          <img src={userData.avatar} alt="profile" />
+          <img 
+          src={userData.avatar} 
+          alt="profile"
+          onClick={handleProfileClick}
+          />
         </div>
         <div className="card__nav__likes">
           <span>{getNumberLikes(userData)}M</span>
@@ -40,12 +52,10 @@ const Card = ({userId}) => {
         </div>
       </nav>
       <section className="card__information">
-        <span className="card__information-name">
-          {userData.nombre}
-        </span>
+        <span className="card__information-name">{userData.nombre}</span>
         <span className="card__information-welcome-message">
           {userData.descripcion}
-          </span>
+        </span>
         <span className="card__information-description">
           Follow me and like my post
         </span>
@@ -54,11 +64,27 @@ const Card = ({userId}) => {
         <button className="card__buttons">Follow</button>
         <button className="card__buttons">Messages</button>
       </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal__container">
+            <div className="modal__container__header">
+              <span className="modal__container__header-name">{userData.nombre}</span>
+              <span className="modal__container__header-description">{userData.descripcion}</span>
+            </div>
+            <div className="modal__container__body">
+              <img src={userData.avatar} alt="profile"/>
+            </div>
+            <div className="modal__container__footer">
+              <button onClick={handleProfileClick}>Close</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 Card.propTypes = {
   userId: PropTypes.number.isRequired,
-}
+};
 
 export default Card;
