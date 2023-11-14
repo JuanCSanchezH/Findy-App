962ca0c326a33ac93b186c380316c35f20b11333
veroa8 <veronica.tangarife1@udea.edu.co>
2023-11-13 22:57:15 -0500

    Final changes

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -4,9 +4,7 @@ const endpoints = {
     userByEmailAndPassword: (email, password) => `${URL_BASE}users?correo=${email}&&contrasena=${password}`,
     users: `${URL_BASE}users`,
     findUserByEmail: (email) => `${URL_BASE}users?correo=${email}`,
-    posts: `${URL_BASE}posts`,
     userByPost:(id) => `${URL_BASE}users?id=${id}`,
-    addOrRemoveLike: (ID) => `${URL_BASE}posts/${ID}`
     userById: (ID) => `${URL_BASE}users/${ID}`,
     postById: (ID) => `${URL_BASE}posts?id=${ID}`,
     addComment: (ID) => `${URL_BASE}users/${ID}/comentarios`,
